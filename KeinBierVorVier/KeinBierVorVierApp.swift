b8cda981ed849040e398a82ced88764da686bbe1
Stefan Lang <stefan.lang@rtl.de>
2024-08-09 10:32:32 +0200

    Initial Commit

---
+++ KeinBierVorVier/KeinBierVorVierApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeinBierVorVierApp.swift
+//  KeinBierVorVier
+//
+//  Created by Lang, Stefan [RTL Tech] on 09.08.24.
+//
+
+import SwiftUI
+
+@main
+struct KeinBierVorVierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
